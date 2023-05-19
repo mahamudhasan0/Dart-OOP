0da96ab9eab2fde7b2295b56c9d528d3f3e8486b
Mahamud Hasan <mahamudhasan960458@gmail.com>
2023-05-20 02:43:04 +0600

    Constructor Practice code

--- Constructor/Constructor.dart
+++ Constructor/Constructor.dart
@@ -2,13 +2,23 @@ class Person{
   String? name;
   int? age;
 
+  //person(this.name, this.age); ==This is single line constructor
+  //person(this.name, [this.age=0]); ==This is called optional parameter also called default value
+  //person({required this.name, required this.age}); ==This is called named parameter
+  // Named parameter value should be passed like Person(name:"example", age:example)
+
   Person(String n, int a){
     this.name = n;
     this.age = a;
   }
+  
+  void display(){
+    print("Name $name");
+    print("Age $age");
+  }
 }
 void main(){
   Person person = Person("Mahamud Hasan", 24);
-  print(person.name);
-  print(person.age);
+  person.display();
+  
 }
